9608ee062a958fb30f298f50de44cea9e838b075
Jack Myers <jackhmyers97@gmail.com>
2022-01-15 11:05:21 -0800

    modified launchers for my rice

--- 1080p/launchers/ribbon/launcher.sh
+++ 1080p/launchers/ribbon/launcher.sh
@@ -19,10 +19,11 @@ styles=($(ls -p --hide="colors.rasi" $dir/styles))
 color="${styles[$(( $RANDOM % 8 ))]}"
 
 # comment this line to disable random colors
-sed -i -e "s/@import .*/@import \"$color\"/g" $dir/styles/colors.rasi
+#sed -i -e "s/@import .*/@import \"$color\"/g" $dir/styles/colors.rasi
 
 # comment these lines to disable random style
-themes=($(ls -p --hide="launcher.sh" --hide="styles" $dir))
-theme="${themes[$(( $RANDOM % 12 ))]}"
+#themes=($(ls -p --hide="launcher.sh" --hide="styles" $dir))
+#theme="${themes[$(( $RANDOM % 12 ))]}"
+theme="ribbon_bottom"
 
 rofi -no-lazy-grab -show drun -modi drun -theme $dir/"$theme"
