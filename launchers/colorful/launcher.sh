8b65bd24719d55fad9592a2b618e5f10c7a533ef
Jack Myers <jackhmyers97@gmail.com>
2022-02-06 14:05:06 -0800

    hemisu theme

--- launchers/colorful/launcher.sh
+++ launchers/colorful/launcher.sh
@@ -16,9 +16,9 @@ dir="$HOME/.config/rofi/launchers/colorful"
 
 # dark
 ALPHA="#00000000"
-BG="#1D2021ff"
-FG="#ebdbb2ff"
-SELECT="#1D2021ff"
+BG="#edededff"
+FG="#000000ff"
+SELECT="#edededff"
 
 # light
 #ALPHA="#00000000"
@@ -30,7 +30,7 @@ SELECT="#1D2021ff"
 COLORS=('#EC7875' '#61C766' '#FDD835' '#42A5F5' '#BA68C8' '#4DD0E1' '#00B19F' \
 		'#FBC02D' '#E57C46' '#AC8476' '#6D8895' '#EC407A' '#B9C244' '#6C77BB')
 #ACCENT="${COLORS[$(( $RANDOM % 14 ))]}ff"
-ACCENT=#d3869b
+ACCENT=#b576bc
 
 # overwrite colors file
 cat > $dir/colors.rasi <<- EOF
